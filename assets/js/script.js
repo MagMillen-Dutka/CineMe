b9b3205fb907d938d8399ded30f5a43265addbee
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-11-09 21:37:49 +0000

    Created search input bar in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
 
-// This coding is JQuery for pulling API into code.
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://movie-search-and-listing-api1.p.rapidapi.com/list_movies.json?limit=10&page=1&quality=All&minimum_rating=8&query_term=0&genre=All&sort_by=date_added&order_by=desc&with_rt_ratings=true',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4d043d3c5dmsh97c286e5b040044p104743jsn0014620381b9',
-		'X-RapidAPI-Host': 'movie-search-and-listing-api1.p.rapidapi.com'
-	}
-};
+// This coding is for the search input by Obi
+    var searchForm = document.getElementById('#Form');
+    var searchInput = document.getElementById('#Input');
+    var searchResults = document.getElementById('#Results');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const searchQuery = searchInput.value;
+     
+    });
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+
+// This coding is JQuery for pulling API into code.
+// const settings = {
+// 	async: true,
+// 	crossDomain: true,
+// 	url: 'https://movie-search-and-listing-api1.p.rapidapi.com/list_movies.json?limit=10&page=1&quality=All&minimum_rating=8&query_term=0&genre=All&sort_by=date_added&order_by=desc&with_rt_ratings=true',
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '4d043d3c5dmsh97c286e5b040044p104743jsn0014620381b9',
+// 		'X-RapidAPI-Host': 'movie-search-and-listing-api1.p.rapidapi.com'
+// 	}
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
 
 // OR fetch API coding below as alternative
 
 function findFilm(event) {
+    
     // This event is needed to stop page refreshing and clearing input. Do not remove.
     event.preventDefault();
     var moviePull = cityInputEl.value;
@@ -25,7 +37,6 @@ function findFilm(event) {
 }
 
 function searchResponse(){
-
 const url = 'https://movie-search-and-listing-api1.p.rapidapi.com/list_movies.json?limit=10&page=1&quality=All&minimum_rating=8&query_term=0&genre=All&sort_by=date_added&order_by=desc&with_rt_ratings=true';
 const options = {
 	method: 'GET',
@@ -36,8 +47,8 @@ const options = {
 };
 
 try {
-	const response = await fetch(url, options);
-	const result = await response.text();
+	const response = await.fetch(url, options);
+	const result = await.response.text();
 	console.log(result);
 } catch (error) {
 	console.error(error);
