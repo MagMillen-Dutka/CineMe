e9d59b6bef430ae51c8bb6f28bfa2eb243552d4d
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-11-13 17:49:28 +0000

    Search-Engine first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+var searchForm = document.getElementById('searchForm');
+var searchInput = document.getElementById('searchInput');
+var searchResults = document.getElementById('searchResults');
+var tvMazeAPIEl = 'Jdep9pFBwdLLZclOFukZ_VSdjGBpRrLw ';
+
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const searchQuery = searchInput.value;
+
+  // Fetch movie data from API
+  fetch(`https://api.tvmaze.com/search/shows?q=${searchQuery}`)
+    .then(response => response.json())
+    .then(data => {
+      // Filter the movie data based on the search query
+      const filteredResults = data.filter(result => result.show.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+      // Display the filtered movie results to the user
+      searchResults.innerHTML = '';
+      filteredResults.forEach(result => {
+        const resultElement = document.createElement('div');
+        resultElement.textContent = result.show.name;
+        searchResults.appendChild(resultElement);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
